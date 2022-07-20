9675da68c51af68b3b16df8d5a923d368e6d3da0
xuyiwen <523272086@qq.com>
2022-07-20 15:58:59 +0800

    pingpong

---
+++ go.mod
@@ -0,0 +1,5 @@
+module discord
+
+go 1.16
+
+require github.com/bwmarrin/discordgo v0.25.0
---
+++ pingpong/pingpong.go
@@ -0,0 +1,4 @@
+package main
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
