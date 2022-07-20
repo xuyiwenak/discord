9683f928df0a390f5f3abd1c82c0e894dc1fa246
xuyiwen <523272086@qq.com>
2022-07-20 17:47:15 +0800

    ping create channel

--- pingpong/main.go
+++ pingpong/main.go
@@ -16,6 +16,7 @@ var (
 )
 
 // U2FsdGVkX19sfOsKZq6kSaeHjHKisBcqRllTmk3yZshjnCOYCttaC4AwPH5ogj6Q4iUqCwr8ZSw4DwrRqrVPuvnvzD+hGVqR1YUtfvu376Wm3Uikxop7QYDJujtVdaCj
+// go run main.go -t
 func init() {
 
 	flag.StringVar(&Token, "t", "", "Bot Token")
@@ -32,7 +33,7 @@ func main() {
 
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
-
+	dg.AddHandler(messageCreateChannel)
 	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -68,5 +69,57 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+//
+// It is called whenever a message is created but only when it's sent through a
+// server as we did not request IntentsDirectMessages.
+
+func messageCreateChannel(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// In this example, we only care about messages that are "ping".
+	if m.Content != "ping" {
+		return
+	}
+
+	// We create the private channel with the user who sent the message.
+	// 会话创建频道
+	channel, err := s.UserChannelCreate(m.Author.ID)
+	if err != nil {
+		// If an error occurred, we failed to create the channel.
+		//
+		// Some common causes are:
+		// 1. We don't share a server with the user (not possible here).
+		// 2. We opened enough DM channels quickly enough for Discord to
+		//    label us as abusing the endpoint, blocking us from opening
+		//    new ones.
+		fmt.Println("error creating channel:", err)
+		s.ChannelMessageSend(
+			m.ChannelID,
+			"Something went wrong while sending the DM!",
+		)
+		return
+	}
+	// Then we send the message through the channel we created.
+	_, err = s.ChannelMessageSend(channel.ID, "Pong!")
+	if err != nil {
+		// If an error occurred, we failed to send the message.
+		//
+		// It may occur either when we do not share a server with the
+		// user (highly unlikely as we just received a message) or
+		// the user disabled DM in their settings (more likely).
+		fmt.Println("error sending DM message:", err)
+		s.ChannelMessageSend(
+			m.ChannelID,
+			"Failed to send you a DM. "+
+				"Did you disable DM in your privacy settings?",
+		)
 	}
 }
